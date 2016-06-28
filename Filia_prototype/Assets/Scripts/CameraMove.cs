39bb99497605aca753eddba652dc990264ec11be
Stanislav <stas.mishchenko@gmail.com>
2016-06-28 18:02:15 +0200

    update28_06
    Mesh collider 1st floor
    player+player move
    camera move
    camera post effect
    shader toon outline settings

---
+++ Filia_prototype/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+    public GameObject player;
+    public Vector3 offset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        offset = transform.position - player.transform.position;
+
+    }
+
+        // Update is called once per frame
+
+        void LateUpdate () {
+
+         //   float moveX = Input.GetAxis("Horizontal");
+          //  float moveZ = Input.GetAxis("Vertical");
+
+          //  offset = new Vector3(20, moveZ, moveX);
+       
+            transform.position = player.transform.position + offset;
+	
+	}
+}
---
+++ Filia_prototype/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float maxSpeed = 5.0f;
+
+    private bool facingRight = true;
+    private Animator anim;
+    private CharacterController _charController;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        _charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+
+
+    void Update() {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        if (moveX != 0)
+            anim.SetFloat("Speed", Mathf.Abs(moveX));
+        else
+            anim.SetFloat("Speed", Mathf.Abs(moveZ));
+
+        Vector3 move = new Vector3(moveX * maxSpeed, 0, moveZ * maxSpeed);
+
+        move = Vector3.ClampMagnitude(move, maxSpeed);
+        move *= Time.deltaTime;
+
+        move = transform.TransformDirection(move);
+        _charController.Move(move);
+
+        if (moveX > 0 && !facingRight)
+            Flip();
+        else if (moveX < 0 && facingRight)
+            Flip();
+    }
+
+    private void Flip() {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+    }
+}
